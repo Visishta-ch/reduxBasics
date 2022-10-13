4606955f7b8b80a08b14387285010ac8425a400c
Visishta-ch <Visishtachatla@gmail.com>
2022-10-13 23:35:30 +0530

    counter usind redux

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import {createStore} from 'redux';
+ 
+/*steps  1: define reducer 2: create store 3: component that subscribes the store nd finally dispatching the action */
+
+
+const reducer = (state = {counter : 0}, action) =>{
+    if(action.type === 'increment'){
+        return {
+            counter : state.counter + 2,
+        };
+        
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter : state.counter - 2,
+        }
+        
+    }
+    return state;
+}
+const store = createStore(reducer);
+
+
+// const subscriberCount = () => {
+//     const latestState = store.getState();
+//     console.log(latestState)
+// }
+// store.subscribe(subscriberCount);
+// store.dispatch({type:'increment'})
+
+export default store;
